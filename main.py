76cd8c14e436943f9c976113bbc9b83375f3fe78
johare <johare@mines.edu>
2024-05-01 16:45:09 -0600

    Food separated from main

--- main.py
+++ main.py
@@ -1,37 +1,12 @@
-import pygame
 import random
-from drawable import Drawable
 from simulation import Simulation
 from constants import Constants
-import numpy as np
+from food import FoodHerbivore
 
-from monteCarlo import monteCarlo
 from blob import Blob
 
 
  
-
-class Food:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-        
-
-class FoodHerbivore(Food, Drawable):
-    def __init__(self, x, y):
-        super().__init__(x, y)
-        self.size = random.randint(3, 5)
-        self.color = (0, 220, 0)
-        self.rooted = True
-    
-
-    def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
-        pygame.draw.circle(screen, Constants.BLACK, (self.x, self.y), self.size, 1)
-    
-
-
 
 if __name__ == "__main__":
     simulation = Simulation()
