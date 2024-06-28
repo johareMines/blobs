1144561a0ef04cfb1c006a79eb55051a60140aca
johare <johare@mines.edu>
2024-06-28 17:15:20 -0600

    Work on spider

--- spider.py
+++ spider.py
@@ -95,6 +95,7 @@ class Spider(Organism):
         if self.webJuice < self.MINIMUM_JUICE_TO_SPIN:
             return self.randomWalk()
         
+        # Check silk level and determine how far is possible
         maxNodes = int(self.juice // self.JUICE_PER_NODE)
         
         maxWebDist = maxNodes * self.NODE_DISTANCE
@@ -123,13 +124,89 @@ class Spider(Organism):
                     
                     potentialNodes.append(node)
         
+        
+        ## Spin web ##
+        webDestX = None
+        webDestY = None
+        
         # Spin random web    
         if potentialNodes == []:
-            targetDist = abs(maxWebDist * monteCarlo("GREATER")[0])d
+            monteCarlo1 = monteCarlo("GREATER")
+            # targetDist = abs(maxWebDist * monteCarlo1[0])
+            
+            # Choose coordinate less than maxWebDist away
+            maxWebDistSqrt = math.sqrt(maxWebDist)
+            dx, dy = (random.randint(-maxWebDistSqrt, maxWebDistSqrt) for _ in range(2))
+            
+            debugDist = math.sqrt(dx ** 2 + dy ** 2)
+            print("Random web, distance: {} | Max Web Distance: {}".format(debugDist, maxWebDist))
+            
+            
+            webDestX = self.x + dx
+            webDestY = self.y + dy
+            
+            
+            
+            
+            
         index = random.randint(0, len(potentialNodes)-1)
             
     
-   
+    def shootWebStrand(self, destX, destY):
+        # Calculate distance between start and end points
+        dist = math.sqrt((destX - self.x) ** 2 + (destY - self.y) ** 2)
+
+        # Calculate the direction vector
+        dx = destX - self.x
+        dy = destY - self.y
+
+        # Normalize the direction vector
+        unit_dx = dx / dist
+        unit_dy = dy / dist
+
+        # Check if spider is on existing node
+        # TODO: implement quadtree to make this query better than O(n^2) (Currently iterates through every node in the web @ worst case scenario, loops through every node of every strand)
+        currentNode = None
+        nodeFound = False
+        for strand in self.web:
+            if nodeFound:
+                break
+            for node in strand:
+                nodeDist = math.sqrt((node.x - self.x) ** 2 + (node.y - self.y) ** 2)
+                
+                # Spider is close enough to existing node
+                if nodeDist <= 5:
+                    currentNode = node
+                    nodeFound = True
+                    break
+        
+        nodes = []
+        if currentNode is not None:
+            nodes.append(currentNode)
+        else:
+            nodes.append(Node(self.x, self.y))
+        nodes = [(self.x, self.y)]
+
+        # Step size (distance between nodes)
+        step_size = 50
+
+        # Add nodes every 50 units
+        num_steps = int(dist // step_size)
+        for step in range(1, num_steps + 1):
+            node_x = self.x + step * step_size * unit_dx
+            node_y = self.y + step * step_size * unit_dy
+            nodes.append((node_x, node_y))
+
+        # Add the final destination node
+        nodes.append((destX, destY))
+
+        return nodes
+        
+        
+    def addNodesRecursively(self, endNode, dist=-1):
+       
+       # When passed in a dist:
+       numSteps = int(dist // Spider.NODE_DISTANCE)
             
             
     # Figure out where to move to and how to do it, then do it
@@ -149,4 +226,4 @@ class Spider(Organism):
         
     
     def die(self):
-        print("todo implement spider die")
+        print("todo implement spider die") 
